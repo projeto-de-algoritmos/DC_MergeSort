5d60a0d827d43fdd3415d37b240f6642da015677
joaovitorml <joao.lemos4000@gmail.com>
2019-11-04 20:26:10 -0300

    Creating a new functional code.
    Co-authored-by: Heron <heronildo.2010@hotmail.com>

--- count_inv.py
+++ count_inv.py
@@ -6,58 +6,44 @@ INVERSIONS_SORT = 0
 INVERSIONS_BRUTE = 0
 
 
-
-def mergeArray(start, split, end, masterArray, tempArray):
-
+# Python program for implementation of MergeSort 
+def mergeSort(arr):
     global INVERSIONS_SORT
-    leftCount = start
-    rightCount = split+1
-    tempCount = start
+    if len(arr) >1: 
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+  
+        mergeSort(L)
+        mergeSort(R)
+  
+        i = j = k = 0
+          
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j]
+                INVERSIONS_SORT = INVERSIONS_SORT + (len(L) + 1 - j);
+                j+=1
+            k+=1
 
-    while((leftCount <= split) and (rightCount <= end)):
-        print('leftCount: ', leftCount)
-        print('rightCount: ', rightCount)
-        print('tempCount: ', tempCount)
-        print('\n')
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
 
-        if(masterArray[leftCount] <= masterArray[rightCount]):
-            tempArray[tempCount] = masterArray[leftCount]
-            leftCount += 1
-            tempCount += 1
-            
-        else:
-            tempArray[tempCount] = masterArray[rightCount]
-            INVERSIONS_SORT += (split + 1 - leftCount)
-            rightCount +=1
-            tempCount +=1
-    i = rightCount
-    j = leftCount
-    while(i < end):
-            tempArray[tempCount] = masterArray[i]
-            tempCount +=1
-            i +=1
-
-    while(j < split):
-            tempArray[tempCount] = masterArray[j]
-            j +=1
-            tempCount +=1
-
-    print('tempArray: ',tempArray)
-
-    masterArray = tempArray
-    print('masterArray: ',masterArray)
-
-
-def sortArray(start, end, masterArray, tempArray):
-    split = (start + end)//2
-
-    if(start < end):
-        sortArray(start, split, masterArray, tempArray)
-        sortArray(split + 1, end, masterArray, tempArray)
-        mergeArray(start, split, end, masterArray, tempArray)
-        print(masterArray)
-
-
+def printList(arr): 
+    for i in range(len(arr)):         
+        print(arr[i],end=" ") 
+    print() 
+  
 def bruteCount(masterArray):
     global INVERSIONS_BRUTE
     i=0
@@ -72,7 +58,6 @@ def bruteCount(masterArray):
 size = int(input('Informe o tamanho do vetor: '))
 
 masterArray = []
-tempArray = []
 
 if(size < 1000000):
     for i in range(size):
@@ -87,7 +72,7 @@ fim1 = time.time()
 
 inicio2 = time.time()
 print('size: ',size)
-sortArray(0, size-1, masterArray, tempArray)
+mergeSort(masterArray)
 fim2 = time.time()
 
 print('\n')
@@ -99,6 +84,6 @@ print('Numero de inversoes por forca bruta: ', INVERSIONS_BRUTE)
 print('Tempo por forca bruta: ', fim1 - inicio1)
 print('\n')
 
-print('Numero de inversoes pelo merge sort', INVERSIONS_SORT)
+print('Numero de inversoes pelo merge sort: ', INVERSIONS_SORT)
 print('Tempo pelo merge sort: ', fim2 - inicio2)
 print('\n')
