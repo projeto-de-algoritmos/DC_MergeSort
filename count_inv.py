ce6623fb82caad43b12ac4b39a1d784c86f6a96f
joaovitorml <joao.lemos4000@gmail.com>
2019-11-04 12:08:13 -0300

    Fixed some problems.
    Co-authored-by: Heron <heronildo.2010@hotmail.com>

--- count_inv.py
+++ count_inv.py
@@ -10,39 +10,40 @@ def mergeArray(start, split, end, masterArray, tempArray):
     global INVERSIONS_SORT
     leftCount = start
     rightCount = split+1
-    tempCount = start
 
     while((leftCount <= split) and (rightCount <= end)):
-        if(masterArray[leftCount] <= masterArray[rightCount]):
-            tempArray[tempCount] = masterArray[leftCount]
+        print('leftCount: ', leftCount)
+        print('rightCount: ', rightCount)
+        print('\n')
+        if(masterArray[leftCount] < masterArray[rightCount]):
+            tempArray.append(masterArray[leftCount])
             leftCount += 1
         else:
-            tempArray[leftCount] = masterArray[rightCount]
+            tempArray.append(masterArray[rightCount])
             INVERSIONS_SORT += (split + 1 - leftCount)
             rightCount +=1
-        tempCount +=1
     
-    if(leftCount > split):
-        while(rightCount < end):
-            tempArray[tempCount] = masterArray[rightCount]
-            tempCount +=1
+    while(rightCount < end):
+            tempArray.append(masterArray[rightCount])
             rightCount +=1
-    else:
-        while(leftCount < split):
-            tempArray[tempCount] = masterArray[leftCount]
-            tempCount +=1
+    while(leftCount < split):
+            tempArray.append(masterArray[leftCount])
             leftCount +=1
 
+    print('tempArray: ',tempArray)
+
     masterArray = tempArray
+    print('masterArray: ',masterArray)
 
 
 def sortArray(start, end, masterArray, tempArray):
-    split = (start + end)/2
+    split = (start + end)//2
 
     if(start < end):
         sortArray(start, split, masterArray, tempArray)
         sortArray(split + 1, end, masterArray, tempArray)
         mergeArray(start, split, end, masterArray, tempArray)
+        print(masterArray)
 
 def bruteCount(masterArray):
     global INVERSIONS_BRUTE
@@ -63,16 +64,16 @@ tempArray = []
 if(size < 1000000):
     for i in range(size):
        masterArray.append(random.randint(0,1000))
-       continue
 
 if(size < 20):
         print(masterArray)
 
 inicio1 = time.time()
-bruteCount(masterArray)
+# bruteCount(masterArray)
 fim1 = time.time()
 
 inicio2 = time.time()
+print('size: ',size)
 sortArray(0, size - 1, masterArray, tempArray)
 fim2 = time.time()
 
@@ -85,6 +86,6 @@ print('Numero de inversoes por forca bruta: ', INVERSIONS_BRUTE)
 print('Tempo por forca bruta: ', fim1 - inicio1)
 print('\n')
 
-print('Numero de inversoes pelo merge sort')
+print('Numero de inversoes pelo merge sort', INVERSIONS_SORT)
 print('Tempo pelo merge sort: ', fim2 - inicio2)
 print('\n')
